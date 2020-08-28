20cfc6bfbe5f83b7a79f6f2fafea5452f12254ae
Mohamed Edamer <edamer.mo@gmail.com>
2020-08-27 20:40:30 -0700

    fixed pkl file

--- app/api/model.py
+++ app/api/model.py
@@ -11,7 +11,7 @@ import pandas as pd
 import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
-from joblib import dump, load
+import pickle
 import en_core_web_sm
 from spacy import load
 
@@ -30,23 +30,30 @@ def tokenizer(text):
 model = TfidfVectorizer(stop_words = 'english',
                         ngram_range = (1,2),
                         max_df = .95,
-                        min_df = 3)
-                        #tokenizer = tokenizer)
+                        min_df = 3,
+                        tokenizer = tokenizer)
 
-# # Fit and transform the data:
-# dtm = model.fit_transform(df['Effects'])
+# Fit and transform the data:
+dtm = model.fit_transform(df['Effects'])
 
-# # Get features:
-# dtm = pd.DataFrame(dtm.todense(), columns = model.get_feature_names())
+# Get features:
+dtm = pd.DataFrame(dtm.todense(), columns = model.get_feature_names())
 
-# nn = NearestNeighbors(n_neighbors=5, algorithm='kd_tree')
-# nn.fit(dtm)
+nn = NearestNeighbors(n_neighbors=5, algorithm='kd_tree')
+nn.fit(dtm)
 
-# dump(nn, 'model.joblib')
+filename = 'model.pkl'
+# Open the file to save as pkl file
+model_pkl = open(filename, 'wb')
+pickle.dump(nn, model_pkl)
+# Close the pickle instances
+model_pkl.close()
 
 
 def train(user_input):
-    nn2 = load('model.joblib')
+    model_pkl = open(filename, 'rb')
+    nn2 = pickle.load(model_pkl)
+    #nn2 = load('model.joblib')
     sample = [user_input]
     vec = model.transform(sample)
     dense = vec.todense()
@@ -61,69 +68,3 @@ def train(user_input):
 
     return output[0]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-########
-def train(user_input):
-    df = pd.read_csv('Data/cannabis_new.csv')
-
-    nlp= en_core_web_sm.load()
-
-
-    def tokenizer(text):
-        doc=nlp(text)
-        return [token.lemma_ for token in doc if ((token.is_stop == False) and
-        (token.is_punct == False)) and (token.pos_ != 'PRON')]
-
-    # Build the model:
-    model = TfidfVectorizer(stop_words = 'english',
-                           ngram_range = (1,2),
-                           max_df = .95,
-                           min_df = 3,
-                           tokenizer = tokenizer)
-
-    # Fit and transform the data:
-    dtm = model.fit_transform(df['Effects'])
-
-    # Get features:
-    dtm = pd.DataFrame(dtm.todense(), columns = model.get_feature_names())
-
-    nn = NearestNeighbors(n_neighbors=5, algorithm='kd_tree')
-    nn.fit(dtm)
-
-    # Fin similar strains and return the strain_id:
-    # Victorise the text:
-    sample = [user_input]
-    vec = model.transform(sample)
-    # find similar effects:
-    dense = vec.todense()
-    similar = nn.kneighbors(dense, return_distance=False)
-    similar.T
-
-    output = []
-    for i in range(5):
-        elem = similar[0][i]
-        output.append(elem)
-
-
-    return output[0]
